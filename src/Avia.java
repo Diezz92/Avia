27534d9ebe156938badbdc03f3778ff7222f4d24
Станислав <diezz_92@mail.ru>
2022-07-10 23:25:19 +0500

    Add

---
+++ src/Avia.java
@@ -0,0 +1,8 @@
+public class Avia {
+    public static void main(String[] args) {
+        int price = 1000;
+        int mile = 20;
+        int bonus = price / mile;
+        System.out.println("Бонусные мили: " + bonus);
+    }
+}
